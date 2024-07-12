1c8e6f40420c2934b302e60c56708713fcb9f86c
Noam Avitan <noamavitan159753@gmail.com>
2024-07-12 12:51:39 +0300

    delete uri

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Noam:Noam159753852@cluster0.1juvzwi.mongodb.net/maoz"
+     "mongodburi"
     );
     console.log("connected to mongoDB");
   } catch (error) {
